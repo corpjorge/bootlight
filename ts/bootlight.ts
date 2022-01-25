db412747663edbd3e06b1c068ca63d2fef4e325b
Jorge Eduardo <corpjorge@hotmail.com>
2022-01-25 12:35:26 -0500

    colores y boton js

--- ts/bootlight.ts
+++ ts/bootlight.ts
@@ -1,1 +1,10 @@
-console.log("d");
+document.getElementById("dark-mode").onclick = function () {
+  const body = document.getElementsByClassName("dark");
+  const element = document.getElementsByTagName("body");
+
+  if (body.length > 0) {
+    return element[0].classList.remove("dark");
+  }
+
+  return element[0].classList.add("dark");
+};
