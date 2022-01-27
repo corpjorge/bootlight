e90ffdb8c35a19a716d2b02449ed497758b11677
Jorge Eduardo <corpjorge@hotmail.com>
2022-01-26 23:09:27 -0500

    summer

--- dist/js/bootlight.js
+++ dist/js/bootlight.js
@@ -1,9 +1,9 @@
 document.getElementById("dark-mode").onclick = function () {
-    var body = document.getElementsByClassName("dark");
+    var body = document.getElementsByClassName("dark-mode-on");
     var element = document.getElementsByTagName("body");
     if (body.length > 0) {
-        return element[0].classList.remove("dark");
+        return element[0].classList.remove("dark-mode-on");
     }
-    return element[0].classList.add("dark");
+    return element[0].classList.add("dark-mode-on");
 };
 //# sourceMappingURL=bootlight.js.map
--- ts/bootlight.ts
+++ ts/bootlight.ts
@@ -1,10 +1,9 @@
 document.getElementById("dark-mode").onclick = function () {
-  const body = document.getElementsByClassName("dark");
+  const body = document.getElementsByClassName("dark-mode-on");
   const element = document.getElementsByTagName("body");
-
   if (body.length > 0) {
-    return element[0].classList.remove("dark");
+    return element[0].classList.remove("dark-mode-on");
   }
 
-  return element[0].classList.add("dark");
+  return element[0].classList.add("dark-mode-on");
 };
